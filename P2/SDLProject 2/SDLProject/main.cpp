d3af830fb81549f586e1269bdedf8d8c33d507cd
Thomas Molina <tm3083@nyu.edu>
2021-03-03 10:15:00 -0500

    newer version

--- P2/SDLProject 2/SDLProject/main.cpp
+++ P2/SDLProject 2/SDLProject/main.cpp
@@ -10,20 +10,22 @@
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
+#include <vector>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #include "Entity.h"
-
+#define BAR_COUNT 9
 
 
 struct GameState{
     Entity *leftPad;
     Entity *square;
     Entity *rightPad;
-    
+    Entity *bars;
 };
+
 GameState state;
 
 SDL_Window* displayWindow;
@@ -33,6 +35,7 @@ ShaderProgram program;
 glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
 
 
+
 //TEXTURES!
 GLuint LoadTexture(const char* filePath) {
     int w, h, n;
@@ -40,7 +43,8 @@ GLuint LoadTexture(const char* filePath) {
     if (image == NULL) {
         std::cout << "Unable to load image. Make sure the path is correct\n";
         assert(false);
-}
+    }
+    
     GLuint textureID;
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
@@ -81,20 +85,56 @@ void Initialize() {
     
     glUseProgram(program.programID);
     
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.5f, 0.8f, 0.0f);
     glEnable(GL_BLEND);
     
     // Good setting for transparency
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
     //Textures!!
-   
     state.square = new Entity();
     
     GLuint squareTextureID = LoadTexture("square.png");
     state.square->textureID = squareTextureID;
     state.square->position = glm::vec3(0.0f, 0.0f, 0.0f);
     state.square->speed = 2.0f;
+    
+    
+    state.bars = new Entity[BAR_COUNT];
+    GLuint barsTextureID = LoadTexture("bar.png");
+    
+    state.bars[0].textureID = barsTextureID;
+    state.bars[0].position = glm::vec3(0.0f, 2.0f, 0.0f);
+    
+    state.bars[1].textureID = barsTextureID;
+    state.bars[1].position = glm::vec3(0.0f, 1.0f, 0.0f);
+    
+    state.bars[2].textureID = barsTextureID;
+    state.bars[2].position = glm::vec3(0.0f, 0.0f, 0.0f);
+    
+    state.bars[3].textureID = barsTextureID;
+    state.bars[3].position = glm::vec3(0.0f, 3.0f, 0.0f);
+    
+    state.bars[4].textureID = barsTextureID;
+    state.bars[4].position = glm::vec3(0.0f, -1.0f, 0.0f);
+    
+    state.bars[5].textureID = barsTextureID;
+    state.bars[5].position = glm::vec3(0.0f, -2.0f, 0.0f);
+    
+    state.bars[6].textureID = barsTextureID;
+    state.bars[6].position = glm::vec3(0.0f, -3.0f, 0.0f);
+    
+    state.bars[7].textureID = barsTextureID;
+    state.bars[7].position = glm::vec3(0.0f, -4.0f, 0.0f);
+    
+    state.bars[8].textureID = barsTextureID;
+    state.bars[8].position = glm::vec3(0.0f, 4.0f, 0.0f);
+    
+    state.bars[9].textureID = barsTextureID;
+    state.bars[9].position = glm::vec3(0.0f, 5.0f, 0.0f);
+    
+    
+    
     
     state.leftPad = new Entity();
     state.leftPad->position = glm::vec3(-5.0f, 0.0f, 0.0f);
@@ -109,13 +149,74 @@ void Initialize() {
     state.rightPad->movement = glm::vec3(0);
     state.rightPad->speed = 3.0f;
     
-    
-    
 }
 
+//Text!
+void DrawText(ShaderProgram *program, GLuint fontTextureID, std::string text,
+                                 float size, float spacing, glm::vec3 position)
+{
+    float width = 1.0f / 16.0f;
+    float height = 1.0f / 16.0f;
+    
+    std::vector<float> vertices;
+    std::vector<float> texCoords;
+    
+    for(int i = 0; i < text.size(); i++) {
+        
+        int index = (int)text[i];
+        float offset = (size + spacing) * i;
+        float u = (float)(index % 16) / 16.0f;
+        float v = (float)(index / 16) / 16.0f;
+        
+        vertices.insert(vertices.end(), {
+                offset + (-0.5f * size), 0.5f * size,
+                offset + (-0.5f * size), -0.5f * size,
+                offset + (0.5f * size), 0.5f * size,
+                offset + (0.5f * size), -0.5f * size,
+                offset + (0.5f * size), 0.5f * size,
+                offset + (-0.5f * size), -0.5f * size,
+        });
+        
+        texCoords.insert(texCoords.end(), {
+            u, v,
+            u, v + height,
+            u + width, v,
+            u + width, v + height,
+            u + width, v,
+            u, v + height,
+        });
+        
+    }
+    
+    glm::mat4 modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::translate(modelMatrix, position);
+    program->SetModelMatrix(modelMatrix);
+    
+    glUseProgram(program->programID);
+    
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices.data());
+    glEnableVertexAttribArray(program->positionAttribute);
+    
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords.data());
+    glEnableVertexAttribArray(program->texCoordAttribute);
+    
+    glBindTexture(GL_TEXTURE_2D, fontTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, (int)(text.size() * 6));
+    
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+    
+    }
+//
+//void WinnerRender(){
+//    glClear(GL_COLOR_BUFFER_BIT);
+//
+//    GLuint fontTextureID = LoadTexture("font2.png");
+//    DrawText(&program, fontTextureID, "Winner!" , 0.5f, -0.25f, glm::vec3(-4.0f, 2.0, 0));
+//    //SDL_GL_SwapWindow(displayWindow);
+//}
 
 
-// y = 3.25 is max height and y = -3.25 is min height
 
 void ProcessInput() {
    state.leftPad->movement = glm::vec3(0);
@@ -152,8 +253,12 @@ void ProcessInput() {
                switch (event.key.keysym.sym) {
                    
                    case SDLK_SPACE:
-                       //state.square->speed = 3.0f;
                        state.square->movement = glm::vec3(myX_direc[rand_X], myY_direc[rand_Y],0.0f);
+                       break;
+                   case SDLK_r:
+                       state.square->position = glm::vec3(0);
+                       state.leftPad->position = glm::vec3(-5.0f, 0.0f, 0.0f);
+                       state.rightPad->position = glm::vec3(5.0f, 0.0f, 0.0f);
                        break;
                }
                break; // SDL_KEYDOWN
@@ -172,9 +277,16 @@ void ProcessInput() {
     //Game Over
     if(state.square->position.x >= 4.9f){
         state.square->movement = glm::vec3(0);
+        GLuint fontTextureID = LoadTexture("font2.png");
+        DrawText(&program, fontTextureID, "Winner!" , 0.5f, -0.25f, glm::vec3(-4.0f, 2.0, 0));
+        //WinnerRender();
+        //DrawText(&program, fontTextureID, "Player 1" , 0.5f, -0.25f, glm::vec3(-4.0f, 3.3, 0));
     }
     else if(state.square->position.x <= -4.9f){
         state.square->movement = glm::vec3(0);
+        GLuint fontTextureID = LoadTexture("font2.png");
+        DrawText(&program, fontTextureID, "Winner!" , 0.5f, -0.25f, glm::vec3(4.0f, 2.0, 0));
+        //WinnerRender();
     }
         
     
@@ -244,21 +356,34 @@ void Update() {
     float deltaTime = ticks - lastTicks;
     lastTicks = ticks;
     
+    for(int i = 0; i < BAR_COUNT; i++){
+        state.bars[i].Update(deltaTime);
+    }
 
     state.square->Update(deltaTime);
     state.leftPad->Update(deltaTime);
     state.rightPad->Update(deltaTime);
     
-
+    
 }
 
 //Rendering for Textures!
 void Render() {
     glClear(GL_COLOR_BUFFER_BIT);
     
+    GLuint fontTextureID = LoadTexture("font2.png");
+    DrawText(&program, fontTextureID, "Player 1" , 0.5f, -0.25f, glm::vec3(-4.0f, 3.3, 0));
+    
+    DrawText(&program, fontTextureID, "Player 2" , 0.5f, -0.25f, glm::vec3(2.5f, 3.3, 0));
+    
+    for(int i = 0; i < BAR_COUNT; i++){
+        state.bars[i].Render(&program);
+    }
+    
     state.square->Render(&program);
     state.leftPad->Render(&program);
     state.rightPad->Render(&program);
+    
     
     SDL_GL_SwapWindow(displayWindow);
     }
